45cdb47392c89e9b6972390d79e000e72f55e365
Iro <56658124+IROpany@users.noreply.github.com>
2023-10-15 17:06:35 +0900

    Update category_functions.php

--- slick_slider_plugin/category_functions.php
+++ slick_slider_plugin/category_functions.php
@@ -1,13 +1,10 @@
 <?php
 /*
-Plugin Name: Custom Support Button
-Description: Add a custom support button to your posts.
+Plugin Name: Shortcode Slider Plugin
+Description: Add a slider block to your posts.
 Version: 1.0
 Author: Iro
 */
-
-
-
 
 
 function custom_slider_shortcode() {
@@ -16,14 +13,15 @@ function custom_slider_shortcode() {
   $category = get_term_by('slug', 'category-slug名', 'category');
   $category_id = $category->term_id;
 
-  // カテゴリーに属する最新の4件の記事を取得
+  // カテゴリーに属する最新の3件の記事を取得
   $args = array(
     'post_type' => 'post',
-    'posts_per_page' => 4,
+    'posts_per_page' => 3,
     'cat' => $category_id,
     'orderby' => 'date',
     'order' => 'DESC'
   );
+  
   $query = new WP_Query($args);
 
   // 記事が存在する場合、表示する
@@ -44,9 +42,11 @@ function custom_slider_shortcode() {
 
     // slickのdiv要素を閉じる
     echo '</div>';
-
+    
     // slickのJavaScriptをフックで追加する
     add_action('wp_footer', function() { ?>
+
+
 
       <script type="text/javascript">
         jQuery(document).ready(function() {
@@ -69,39 +69,3 @@ function custom_slider_shortcode() {
 // ショートコードを追加する
 add_shortcode('custom_slider', 'custom_slider_shortcode');
 
-/*
-フックによって追加された部分には、
-コメントで
-// slickのdiv要素をフックで追加するや
-// slickのJavaScriptをフックで追加する
-といったように、フックで追加された部分であることを明示するようにしました。
-
-
-/ slickのJavaScriptをフックで追加する
-    add_action('wp_footer', function() { ?>
-
-この一行を入れることで、wp_footer アクションフックに対して、
-無名関数（コールバック関数）を追加しています。この関数は、
-フックが実行されたときに自動的に呼び出されます。
-具体的には、WordPressがページのフッターをレンダリングするときに、このフックが実行されます。
-
-この無名関数は、slickのJavaScriptを出力するためのコードを含んでおり、
-これによって slick スライダーがページに表示されます。
-wp_footer アクションは、ページが完全にロードされた後に呼び出されるので、
-slickのスクリプトが正常に動作することが保証されます。
-
-つまり、この一行を追加することで、
-slickスライダーを表示するための必要なJavaScriptコードをページのフッターに出力し、
-それを WordPress が自動的に実行することができるようになるため、
-コードの追加が簡単になります。
-
-ショートコードをフック化することで、
-ショートコードが実行されるタイミングでフック関数が実行されるようになり、
-追加の機能を簡単に実装できるようになります。
-
-また、WordPressでは、functions.phpファイルがあれば、自動的に読み込まれます。
-そのため、新しいファイルを作成する必要はありません。
-functions.phpファイルにフック関数を追加することで、
-WordPressの動作を変更することができます。
-
-*/
