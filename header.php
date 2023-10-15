63bdde70b66d1f52468402a98359f36376dd0335
Iro <56658124+IROpany@users.noreply.github.com>
2023-10-16 00:05:19 +0900

    Add files via upload

---
+++ header.php
@@ -0,0 +1,23 @@
+ 
+ 
+ <?php
+
+
+ <!-- Slick Carousel の基本スタイルシートを読み込む -->
+ <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+
+<!-- テーマ内の Slick Carousel スタイルシートを読み込む -->
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/slick/slick.css"/>
+
+<!-- Slick Carousel のデフォルトスタイリングを追加する -->
+<link rel="stylesheet" type="text/css" href="https://iropany.com/joinus/wp-content/themes/cocoon-master/slick/slick-theme.css"/>
+
+/*header.phpファイル名を変更する必要はありません。
+このファイルはWordPressのテーマファイルの一部であり、
+テーマの構造を決定するために必要です。
+このファイル内で使用される外部スタイルシートのファイル名は変更することができますが、
+注意して変更する必要があります。テーマ内のファイルパスやファイル名が変更された場合、
+スタイルシートを正常に読み込めなくなる可能性があるためです。*/
+
+
+?>
