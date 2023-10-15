0124f7321d19b717963bc8c4ca8216375124b192
Iro <56658124+IROpany@users.noreply.github.com>
2023-10-16 00:05:50 +0900

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!-- jQuery の JavaScript ファイルを読み込む -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+
+<!-- Slick Carousel の JavaScript ファイルを読み込む -->
+<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+
+
