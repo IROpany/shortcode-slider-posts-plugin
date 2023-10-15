9ac2af337806befdb9e61bb8080637fd303cc215
Iro <56658124+IROpany@users.noreply.github.com>
2023-10-15 23:25:50 +0900

    Update category_functions.php

--- slick_slider_plugin/category_functions.php
+++ slick_slider_plugin/category_functions.php
@@ -1,71 +1,66 @@
 <?php
-/*
-Plugin Name: Shortcode Slider Plugin
-Description: Add a slider block to your posts.
-Version: 1.0
-Author: Iro
-*/
+/**
+ * Plugin Name: Custom Slider Plugin
+ * Description: Adds a custom slider to your posts.
+ * Version: 1.0
+ * Author: Your Name
+ */
 
-
+// Shortcode for the slider
 function custom_slider_shortcode() {
+  ob_start(); // Start output buffering
+  ?>
 
-  // カテゴリーのIDを取得
-  $category = get_term_by('slug', 'category-slug名', 'category');
-  $category_id = $category->term_id;
+  <div class="slider">
+    <?php
+    // Your existing slider code
+    // ジョイナスカテゴリーのIDを取得
+    $category = get_category_by_slug('カテゴリーname');
+    $category_id = $category->cat_ID;
 
-  // カテゴリーに属する最新の3件の記事を取得
-  $args = array(
-    'post_type' => 'post',
-    'posts_per_page' => 3,
-    'cat' => $category_id,
-    'orderby' => 'date',
-    'order' => 'DESC'
-  );
-  
-  $query = new WP_Query($args);
+    // ジョイナスカテゴリーに属する最新の3件の記事を取得
+    $args = array(
+      'post_type' => 'post',
+      'posts_per_page' => 4,
+      'cat' => $category_id,
+      'orderby' => 'date',
+      'order' => 'DESC'
+    );
+    $query = new WP_Query($args);
 
-  // 記事が存在する場合、表示する
-  if ($query->have_posts()) {
-
-    // slickのdiv要素をフックで追加する
-    echo '<div class="slider">';
-
-    // 記事ループの開始
-    while ($query->have_posts()) {
-      $query->the_post();
-      echo '<div>';
-      echo '<a href="' . get_the_permalink() . '"><img src="' . get_the_post_thumbnail_url() . '"></a>';
-      echo '<p><a href="' . get_the_permalink() . '">' . get_the_date() . ' <span>NEW OPEN</span></a></p>';
-      echo '<p><a href="' . get_the_permalink() . '">' . get_the_title() . '</a></p>';
-      echo '</div>';
+    // 記事が存在する場合、表示する
+    if ($query->have_posts()) {
+      while ($query->have_posts()) {
+        $query->the_post();
+        echo '<div>';
+        echo '<a href="' . get_the_permalink() . '"><img src="' . get_the_post_thumbnail_url() . '"></a>';
+        echo '<p><a href="' . get_the_permalink() . '">' . get_the_date() . ' <span>NEW OPEN</span></a></p>';
+        echo '<p><a href="' . get_the_permalink() . '">' . get_the_title() . '</a></p>';
+        echo '</div>';
+      }
     }
 
-    // slickのdiv要素を閉じる
-    echo '</div>';
-    
-    // slickのJavaScriptをフックで追加する
-    add_action('wp_footer', function() { ?>
+    // クエリのリセット
+    wp_reset_postdata();
+    // ...
+    ?>
+  </div>
 
+  <script type="text/javascript">
+    jQuery(document).ready(function() {
+      jQuery('.slider').slick({
+        autoplay: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+      });
+    });
+  </script>
 
-
-      <script type="text/javascript">
-        jQuery(document).ready(function() {
-          jQuery('.slider').slick({
-            autoplay: true,
-            slidesToShow: 3,
-            slidesToScroll: 1
-          });
-        });
-      </script>
-
-    <?php });
-
-  }
-
-  // クエリのリセット
-  wp_reset_postdata();
+  <?php
+  return ob_get_clean(); // Return the buffered content
 }
 
-// ショートコードを追加する
+// Register the shortcode
 add_shortcode('custom_slider', 'custom_slider_shortcode');
 
+  
