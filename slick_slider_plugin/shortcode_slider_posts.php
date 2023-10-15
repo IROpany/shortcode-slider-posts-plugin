cad18fe9db148f95ad95cc6e3f4cee11e854d2fd
Iro <56658124+IROpany@users.noreply.github.com>
2023-10-16 07:49:16 +0900

    Update shortcode_slider_posts.php

--- slick_slider_plugin/shortcode_slider_posts.php
+++ slick_slider_plugin/shortcode_slider_posts.php
@@ -15,7 +15,7 @@ function custom_slider_shortcode() {
   <div class="slider">
     <?php
     // カテゴリーのIDを取得
-    $category = get_category_by_slug('カテゴリーslug名'); 　// 「カテゴリslug名を入力してください。」
+    $category = get_category_by_slug('カテゴリーslug名'); 　// 「カテゴリ-slug名を入力してください。」
     $category_id = $category->cat_ID;
 
     // カテゴリーに属する最新の3件の記事を取得
